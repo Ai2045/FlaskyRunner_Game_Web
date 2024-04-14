1cbd74eb886a6308dea626d10a5a1a74372ad166
Ai2045 <92918956+Ai2045@users.noreply.github.com>
2024-04-14 19:09:08 +0200

    login page

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = b'super secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///game.db'
 db = SQLAlchemy(app)
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,30 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, abort, session
 from app import app, db
 
 from app.models.db import Users, Scores
 
+
 def setup_routes(app):
     @app.route('/')
-    @app.route('/login')
+    def index():
+        if 'username' in session:
+            return render_template('index.html', username=session['username'])
+        return render_template('index.html')
+    
+    @app.route('/login', methods=['GET', 'POST'])
     def login():
+        if request.method == 'POST':
+            print(request.form)
+            username = request.form['username']
+            password = request.form['password']
+            user = Users.query.filter_by(username=username).first()
+            print(user)
+            print(user.username)
+            if user is not None and password == user.password:
+                session['username'] = user.username
+                return redirect(url_for('index'))
+            else:
+                return abort(401)
         return render_template('login.html')
+    
+        
